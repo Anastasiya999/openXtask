7962a2f1a3c886338cb2d78db6d28d59af7538e6
Anastasiya999 <anapechko@gmail.com>
2022-05-11 08:30:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
+import "./App.css";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <AccessAlarmIcon />
       </header>
     </div>
   );
