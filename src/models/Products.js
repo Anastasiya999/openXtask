e12474db334e5d2fb90c8bf04801f12f46202324
Anastasiya999 <anapechko@gmail.com>
2022-05-12 15:48:17 +0200

    added products model

---
+++ src/models/Products.js
@@ -0,0 +1,30 @@
+export default class Products {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getCategories() {
+    if (this.productsByCategories) return this.productsByCategories;
+    this.productsByCategories = {};
+
+    for (const item of this.items) {
+      if (this.productsByCategories.hasOwnProperty(item.category)) {
+        this.productsByCategories[item.category] += 1;
+      } else {
+        this.productsByCategories[item.category] = 1;
+      }
+    }
+
+    return this.productsByCategories;
+  }
+
+  getPricesById() {
+    if (this.pricesById) return this.pricesById;
+    this.pricesById = {};
+    for (const item of this.items) {
+      this.pricesById[item.id] = item.price;
+    }
+
+    return this.pricesById;
+  }
+}
