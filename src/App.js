61c10883d98205e39aa01ebc4bf4079b39878257
Anastasiya999 <anapechko@gmail.com>
2022-05-12 15:46:01 +0200

    added folder with models

--- src/App.js
+++ src/App.js
@@ -3,25 +3,7 @@ import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <AccessAlarmIcon />
-      </header>
-    </div>
-  );
+  return <div className="App">hello world</div>;
 }
 
 export default App;
