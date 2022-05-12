366a1977a5621b176e47cf058454098939707ab4
Anastasiya999 <anapechko@gmail.com>
2022-05-12 15:47:47 +0200

    added carts model

---
+++ src/models/Carts.js
@@ -0,0 +1,25 @@
+export default class Carts {
+  constructor(carts) {
+    this.carts = carts;
+  }
+
+  getMostExpensive(productsPrices) {
+    let maxValue = -Infinity;
+    let maxId = 0;
+    for (const cart of this.carts) {
+      let products = cart.products;
+      let sum = 0;
+      for (const product of products) {
+        let price = productsPrices[product.productId];
+        sum += product.quantity * price;
+      }
+
+      if (sum > maxValue) {
+        maxValue = sum;
+        maxId = cart.userId;
+      }
+    }
+
+    return { userId: maxId, value: maxValue };
+  }
+}
