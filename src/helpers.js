bdf193fab61972ee17753cff6af6c841e75e1e6f
Anastasiya999 <anapechko@gmail.com>
2022-05-13 23:40:36 +0200

    some changes in naming

--- src/helpers.js
+++ src/helpers.js
@@ -1,12 +1,12 @@
 const pickByFields = (data, fields) => {
   let sampleData = [];
   for (const sample of data) {
-    sampleData.push(_pick(sample, fields));
+    sampleData.push(pick(sample, fields));
   }
   return sampleData;
 };
 
-function _pick(obj, keys) {
+function pick(obj, keys) {
   return Object.assign({}, ...keys.map((key) => ({ [key]: obj[key] })));
 }
 
